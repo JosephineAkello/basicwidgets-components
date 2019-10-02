368a7bfc5c417e99106caa851eee491701c1ab33
mj <onyangomaureen95@gmail.com>
2019-10-02 19:56:50 +0300

    create list item of products

--- lib/shoppinglistitem.dart
+++ lib/shoppinglistitem.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
 
+class Product{
+  const Product({this.name});
+  final String name;
+}
+
+typedef void CartChangedCallback(Product product, bool inCart);
+
 class ShoppingListItem extends StatelessWidget{
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+  : product = product,
+  super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  Color _getColor(context){
+    return inCart ? Colors.black38 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(context){
+    if(!inCart) return null;
+  
+
+  return TextStyle(
+    color: Colors.black38,
+    decoration: TextDecoration.lineThrough,
+  );
+  }
   Widget build(context){
-    return ListTile();
+    return ListTile(
+      onTap: (){
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+        ),
+        title: Text(product.name, style:_getTextStyle(context)),
+    );
   }
 }
